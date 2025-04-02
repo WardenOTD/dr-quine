349ed87d7701e75fa9ca65f4c048affe54fda8c4
Warden <warden@work.com>
2025-04-02 08:55:19 +0800

    Colleen bonus done

--- bonus/Colleen.py
+++ bonus/Colleen.py
@@ -1,11 +1,13 @@
 # some comment
 
 def func(nl, t):
-	string = "# some comment{1}{1}def func(nl, q):{1}{2}string = {3}{4}{3}{1}{2}print(string.format(nl, t, chr(34), string)){1}{2}return 0{1}{1}def main():{1}{2}# another comment{1}{2}func(chr(10), chr(9)){1}{2}return 0"
-	print(string.format(nl, t, chr(34), string))
+	string = "# some comment{0}{0}def func(nl, t):{0}{1}string = {2}{3}{2}{0}{1}print(string.format(nl, t, chr(34), string), end=''){0}{1}return 0{0}{0}def main():{0}{1}# another comment{0}{1}func(chr(10), chr(9)){0}{1}return 0{0}{0}main()"
+	print(string.format(nl, t, chr(34), string), end='')
 	return 0
 
 def main():
 	# another comment
 	func(chr(10), chr(9))
 	return 0
+
+main()
